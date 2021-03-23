469877f01ec215405d5759878007e658cbc68c27
Sthanley Zani <sthanleyy@hotmail.com>
2021-03-22 22:41:36 -0300

    Fechando sidemenu ao clicar fora dele

--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -1,12 +1,19 @@
-$(document).ready(function(){
-    $('.hamburguer').click(function(){
-         $('.sideMenu').css('left', 0);        
-      }
-    )
-    
-    $('.close').click(function(){
-         $('.sideMenu').css('left', '-275px');
-      }
-    )
-  }
-)
+$(document).ready(function () {
+  $('.hamburguer').click(function () {
+    $('.sideMenu').css('left', 0);
+    $('.sideMenu').addClass('open');
+    $('.sideMenu').removeClass('closed');
+  });
+  $('.close').click(function () {
+    $('.sideMenu').css('left', -275);
+    $('.sideMenu').addClass('closed');
+    $('.sideMenu').removeClass('open');
+  });
+  $(document).on('click', function (e) {
+    let check = e.target.closest('#sideMenu');
+    let menuCheck = check ? true : false
+    if ($('#sideMenu').hasClass('open') && menuCheck === false && !e.target.closest('.hamburguer')) {
+      $('.sideMenu').css('left', -275);
+    }
+  });
+})
